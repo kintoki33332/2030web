1144fbcbc8a6879ee02eb3026c65766d0747c281
kintoki33332 <yahyatopp@gmail.com>
2025-01-04 17:23:46 +0300

    script.js

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function redirectToSearchPage() {
         return;
     }
 
-    window.location.href = `/2030web/all/search.html?query=${searchTerm}`;
+    window.location.href = `../../all/search.html?query=${searchTerm}`;
 }
 // حدد مربع الإدخال
 const searchInput = document.getElementById('search');
@@ -129,7 +129,7 @@ function redirectToSearchPage() {
         return;
     }
     // توجيه المستخدم إلى صفحة البحث مع الكلمة المفتاحية
-    window.location.href = "/2030web/all/search.html?query=" + searchTerm;
+    window.location.href = "../../all/search.html?query=" + searchTerm;
 }
 
 
